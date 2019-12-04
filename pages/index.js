f4f8d88f9971722ff53886cf7e10e68e415be67c
kevin zhuang <kevin7441@gmail.com>
2019-12-04 16:07:27 +1100

    fetch internal data from homepage

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,16 @@
+import { useEffect } from 'react';
+import axios from 'axios';
+
 function Home() {
+  useEffect(() => {
+    const getProducts = async () => {
+      const url = 'http://localhost:3000/api/products';
+      let response = await axios.get(url);
+      console.log(response);
+    };
+    getProducts();
+  }, []);
+
   return <>home</>;
 }
 
