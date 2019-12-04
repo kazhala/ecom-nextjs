a44ef1c0908e0b98473bcdcf13925f5095ce45e3
kevin zhuang <kevin7441@gmail.com>
2019-12-04 16:48:50 +1100

    added initialProps and update .gitignore

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,22 @@
 import { useEffect } from 'react';
 import axios from 'axios';
 
-function Home() {
-  useEffect(() => {
-    const getProducts = async () => {
-      const url = 'http://localhost:3000/api/products';
-      let response = await axios.get(url);
-      console.log(response);
-    };
-    getProducts();
-  }, []);
+function Home(props) {
+  const { products } = props;
+  // useEffect(() => {
+  //   getProducts();
+  // }, []);
+  console.log(products);
 
   return <>home</>;
 }
+
+Home.getInitialProps = async () => {
+  // fetch data
+  const url = 'http://localhost:3000/api/products';
+  let response = await axios.get(url);
+  // return response data
+  return { products: response.data };
+};
 
 export default Home;
