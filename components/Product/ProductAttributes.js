0038377a79df2ede009a71d5fbbe38fea0216b26
kevin zhuang <kevin7441@gmail.com>
2019-12-05 23:14:19 +1100

    added remove product function

--- components/Product/ProductAttributes.js
+++ components/Product/ProductAttributes.js
@@ -1,7 +1,22 @@
-import { Header, Button } from 'semantic-ui-react';
+import { useState } from 'react';
+import { Header, Button, Modal } from 'semantic-ui-react';
+import axios from 'axios';
+import baseUrl from '../../utils/baseUrl';
+import { useRouter } from 'next/router';
 
 function ProductAttributes(props) {
-  const { description } = props;
+  const { description, _id } = props;
+  const [modal, setModal] = useState(false);
+
+  const router = useRouter();
+
+  const handleDelete = async () => {
+    const url = `${baseUrl}/api/product`;
+    const payload = { params: { _id } };
+    await axios.delete(url, payload);
+    router.push('/');
+  };
+
   return (
     <>
       <Header as='h3'>About this product</Header>
@@ -10,7 +25,24 @@ function ProductAttributes(props) {
         icon='trash alternate outline'
         color='red'
         content='Delete Product'
+        onClick={() => setModal(true)}
       />
+      <Modal open={modal} dimmer='blurring'>
+        <Modal.Header>Confirm Delete</Modal.Header>
+        <Modal.Content>
+          <p>Are you sure you want to delete this product?</p>
+          <Modal.Actions>
+            <Button content='Cancel' onClick={() => setModal(false)} />
+            <Button
+              negative
+              icon='trash'
+              content='Delete'
+              labelPosition='right'
+              onClick={handleDelete}
+            />
+          </Modal.Actions>
+        </Modal.Content>
+      </Modal>
     </>
   );
 }
