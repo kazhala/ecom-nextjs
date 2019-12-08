9faaba35812f5bbad286d753c9db2b4909a8b7d7
kevin zhuang <kevin7441@gmail.com>
2019-12-08 15:46:48 +1100

    added logout

--- utils/auth.js
+++ utils/auth.js
@@ -14,3 +14,8 @@ export const redirectUser = (ctx, location) => {
     Router.push(location);
   }
 };
+
+export const handleLogout = () => {
+  cookie.remove('token');
+  Router.push('/login');
+};
