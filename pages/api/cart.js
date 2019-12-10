927710b9384e20dce28a3f22bdee7f558a702a69
kevin zhuang <kevin7441@gmail.com>
2019-12-10 19:24:24 +1100

    import product model to cart api route

--- pages/api/cart.js
+++ pages/api/cart.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import jwt from 'jsonwebtoken';
 import Cart from '../../models/Cart';
+import Product from '../../models/Product';
 import connectDb from '../../utils/connectDb';
 
 connectDb();
