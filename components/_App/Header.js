c08d3a3d6d84b6c6c6f9c79da4e532fb70761824
kevin zhuang <kevin7441@gmail.com>
2019-12-10 17:55:13 +1100

    update site title

--- components/_App/Header.js
+++ components/_App/Header.js
@@ -29,7 +29,7 @@ function Header({ user }) {
               src='/static/logo.svg'
               style={{ marginRight: '1em' }}
             />
-            ReactReserve
+            BilibonShop
           </Menu.Item>
         </Link>
         <Link href='/cart'>
