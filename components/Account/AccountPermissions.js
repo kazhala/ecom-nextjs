f40d26d830ef83d5f2f17f8452f33eed8d9833a1
kevin zhuang <kevin7441@gmail.com>
2019-12-10 16:48:41 +1100

    successful change user permission

--- components/Account/AccountPermissions.js
+++ components/Account/AccountPermissions.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import { Header, Checkbox, Table, Icon } from 'semantic-ui-react';
 import baseUrl from '../../utils/baseUrl';
@@ -48,16 +48,38 @@ function AccountPermissions() {
 }
 
 const UserPermission = ({ user }) => {
+  const [admin, setAdmin] = useState(user.role === 'admin');
+
+  const isFirstRun = useRef(true);
+
+  useEffect(() => {
+    if (isFirstRun.current) {
+      isFirstRun.current = false;
+      return;
+    }
+
+    const updatePermission = async () => {
+      const url = `${baseUrl}/api/account`;
+      const payload = { _id: user._id, role: admin ? 'admin' : 'user' };
+      await axios.put(url, payload);
+    };
+    updatePermission();
+  }, [admin, user]);
+
+  const handleChangePermission = () => {
+    setAdmin(prevState => !prevState);
+  };
+
   return (
     <Table.Row>
       <Table.Cell collapsing>
-        <Checkbox toggle />
+        <Checkbox checked={admin} toggle onChange={handleChangePermission} />
       </Table.Cell>
       <Table.Cell>{user.name}</Table.Cell>
       <Table.Cell>{user.email}</Table.Cell>
       <Table.Cell>{user.createdAt}</Table.Cell>
       <Table.Cell>{user.updatedAt}</Table.Cell>
-      <Table.Cell>{user.role}</Table.Cell>
+      <Table.Cell>{admin ? 'admin' : 'user'}</Table.Cell>
     </Table.Row>
   );
 };
